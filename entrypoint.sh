a6e01cce57f84174cd38ec7e0f49f2285787f8a2
Bert De Cock <bertdecock@protonmail.com>
2020-05-03 14:22:51 +0200

    updated cd script

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,4 @@
 #!/bin/sh -l
-cd "$(GITHUB_WORKSPACE)" || echo "Could not find the code. Make sure you did a checkout."
+echo "New version!"
+cd "$GITHUB_WORKSPACE" || echo "Could not find the code. Make sure you did a checkout."
 npx ng build --"$1"
