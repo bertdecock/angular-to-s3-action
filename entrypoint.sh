3ee144c870bd4ac9bc5a3829baeb31df74c7b057
Bert De Cock <bertdecock@protonmail.com>
2020-05-03 17:13:13 +0200

    Logging which folder will be deployed to which bucket

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/sh -l
 bucketName="$1"
 folderName="$2"
+echo "Will deploy the contents of /dist/$folderName to s3://$bucketName"
 cd "$GITHUB_WORKSPACE" || echo "Could not find the code. Make sure you did a checkout of the project."
 npm i
 npx ng build --prod
